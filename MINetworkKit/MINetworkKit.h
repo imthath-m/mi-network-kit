fa9eb44dcd7aa4bce7bcbb6a3fe0f1f7e8cc2677
Imthath M <imthathullah.m@zohocorp.com>
2019-09-09 14:42:43 +0530

    Initial Commit

---
+++ MINetworkKit/MINetworkKit.h
@@ -0,0 +1,19 @@
+//
+//  MINetworkKit.h
+//  MINetworkKit
+//
+//  Created by Imthath M on 09/09/19.
+//  Copyright © 2019 Imthath M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MINetworkKit.
+FOUNDATION_EXPORT double MINetworkKitVersionNumber;
+
+//! Project version string for MINetworkKit.
+FOUNDATION_EXPORT const unsigned char MINetworkKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MINetworkKit/PublicHeader.h>
+
+
