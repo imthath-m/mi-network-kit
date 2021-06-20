11bbdbe4ff29fa783331e52501a1843170f411e7
Imthath <imthath.m@hotmail.com>
2021-06-20 08:10:33 +0530

    add min versions

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MINetworkKit",
+  platforms: [.iOS("13.0"), .watchOS("6.0"), .macOS("15.0")],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
