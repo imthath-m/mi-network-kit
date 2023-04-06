b2028e6c352d3d4f6a86580bb349f3b2fa42ecb4
Imthath <imthath.m@hotmail.com>
2023-04-06 13:45:48 +0530

    Add support for streaming responses using Async Stream

--- Sources/MINetworkKit/MINetworkUploader.swift
+++ Sources/MINetworkKit/MINetworkUploader.swift
@@ -28,7 +28,7 @@ public class MIUploader: NSObject, MINetworkable, URLSessionTaskDelegate {
       }.resume()
   }
 
-  public func uploadData(_ data: Data?, using request: URLRequest, uilonCompletion handler: @escaping (Result<Data, MINetworkError>) -> Void) {
+  public func uploadData(_ data: Data?, using request: URLRequest, onCompletion handler: @escaping (Result<Data, MINetworkError>) -> Void) {
     session.uploadTask(with: request, from: data) { data, response, error in
         handler(self.process(data, response, error))
       }.resume()
@@ -36,5 +36,48 @@ public class MIUploader: NSObject, MINetworkable, URLSessionTaskDelegate {
 
   public func urlSession(_ session: URLSession, task: URLSessionTask, didSendBodyData bytesSent: Int64, totalBytesSent: Int64, totalBytesExpectedToSend: Int64) {
     delegate?.uploaded(fraction: Float(totalBytesSent / totalBytesExpectedToSend))
+  }
+}
+
+public extension MINetworkable {
+  /// The stream ends when one of the returned data can't be decoded to the expected type.
+  func responseStream<AnyDecodable: Decodable>(from request: MIRequestable) throws -> AsyncStream<AnyDecodable> {
+    try responseStream(from: request.urlRequest())
+  }
+
+  /// The stream ends when one of the returned data can't be decoded to the expected type.
+  func responseStream<AnyDecodable: Decodable>(from urlRequest: URLRequest) -> AsyncStream<AnyDecodable> {
+    AsyncStream { continuation in
+      MIStreamer(request: urlRequest).observe { data in
+        do {
+          let result: AnyDecodable = try JSONDecoder().decode(AnyDecodable.self, from: data)
+          continuation.yield(result)
+        } catch {
+          debugPrint("Error \(error.localizedDescription)")
+          continuation.finish()
+        }
+      }
+    }
+  }
+}
+
+/// Use when the endpoint supports streaming of data
+private class MIStreamer: NSObject, URLSessionDataDelegate {
+  let request: URLRequest
+  var dataHandler: ((Data) -> Void)?
+
+  public init(request: URLRequest) {
+    self.request = request
+  }
+
+  lazy public var session: URLSession = URLSession(configuration: .default, delegate: self, delegateQueue: .main)
+
+  public func observe(dataChange: @escaping (Data) -> Void) {
+    session.dataTask(with: request).resume()
+    dataHandler = dataChange
+  }
+
+  public func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+    dataHandler?(data)
   }
 }
